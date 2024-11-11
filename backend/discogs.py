0452be892433fba44072d672d1d669d9c7dd785c
nun1o2that <nuggetindustry@gmail.com>
2024-11-12 00:55:24 +0100

    explore discogs api and possible functions to implement

--- backend/discogs.py
+++ backend/discogs.py
@@ -1,9 +1,33 @@
-import discogs_client
+from discogs_client import Client
+from discogs_client.models import Release, MixedPaginatedList
 
-def create_discogs_client(token):
-    global d
-    d = discogs_client.Client("MusicManager/0.1", user_token=token)
+class DiscogsHelper:
+    def __init__(self, token) -> None:
+        self.d : Client = Client("MusicManager/0.1", user_token=token)
 
-def search(user_query : str):
-    results = d.search(user_query, type="release")
-    print(results[0].genres)
+    # instead of searching for "releases", searching for "master releases"
+    # and then using the main_release property
+    # for the sake of this program, multiple releases of the same album
+    # e.g. in different formats (lp, cd, ...) don't need to be shown
+    def search(self, user_query : str) -> MixedPaginatedList: # ?
+        results : list = self.d.search(user_query, type="master")
+        artists : str = ""
+
+        for r in results:               # r = Master
+            main_r = r.main_release     # main_r = Release
+            artists = self.artists_array_to_comma_separated_string(main_r.artists)
+            print(f"{main_r.id}: {artists} - {main_r.title} {main_r.genres}", sep=",")
+
+    def get_release(self, release_id : int) -> Release:
+        # this on its own only outputs something like
+        # <Release 32182818 'Mg Ultra'>
+        # return d.release(release_id)
+        return self.d.master(release_id).main_release
+
+    def artists_array_to_comma_separated_string(self, array : list) -> str:
+        artists : str = ""
+        for i in range(len(array)):
+            artists += array[i].name
+            if len(array) > 1 and i < len(array) - 1:
+                artists += ", "
+        return artists
